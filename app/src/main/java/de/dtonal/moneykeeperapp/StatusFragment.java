b9942aa78f88040277917824d6ef3b145b9b4ec6
dtonal <dtonal@posteo.de>
2016-10-17 21:14:43 +0200

    Adds StatusFragment implementation.

--- app/src/main/java/de/dtonal/moneykeeperapp/StatusFragment.java
+++ app/src/main/java/de/dtonal/moneykeeperapp/StatusFragment.java
@@ -1,12 +1,36 @@
 package de.dtonal.moneykeeperapp;
 
+import android.app.DialogFragment;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import cz.msebera.android.httpclient.Header;
 
 
 /**
@@ -18,12 +42,36 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class StatusFragment extends Fragment {
+    private ObjectMapper mapper = new ObjectMapper();
+    private static final String TAG = "StatusFragment";
     private static final String ARG_MAIL = "mail";
     private static final String ARG_PASS = "pass";
+
+    DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY);
+    DateFormat dateFormatForApi = new SimpleDateFormat("yyyy/MM/dd", Locale.GERMANY);
+
     private String mMail;
     private String mPass;
 
     private OnFragmentInteractionListener mListener;
+    private TextView mTextStatusDate;
+    private TextView mTextGivenDay;
+    private TextView mTextActualDay;
+    private TextView mTextResultDay;
+    private TextView mTextGivenWeek;
+    private TextView mTextActualWeek;
+    private TextView mTextResultWeek ;
+    private TextView mTextGivenMonth ;
+    private TextView mTextActualMonth;
+    private TextView mTextResultMonth;
+    private TextView mTextDayOfWeek;
+    private TextView mTextDayOfMonth;
+    private ImageView mImageStatusDay;
+    private ImageView mImageStatusWeek;
+    private ImageView mImageStatusMonth;
+    private TextView mTextMonthlyBudget;
+    private TextView mTextDailyBudget;
+    private ProgressDialog progressDialog;
 
     public StatusFragment() {
         // Required empty public constructor
@@ -57,9 +105,106 @@ public class StatusFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mTextStatusDate = (TextView) getView().findViewById(R.id.textStatusDate);
+        mTextGivenDay = (TextView) getView().findViewById(R.id.textGivenDay);
+        mTextActualDay = (TextView) getView().findViewById(R.id.textActualDay);
+        mTextResultDay = (TextView) getView().findViewById(R.id.textResultDay);
+        mTextGivenWeek = (TextView) getView().findViewById(R.id.textGivenWeek);
+        mTextActualWeek = (TextView) getView().findViewById(R.id.textActualWeek);
+        mTextResultWeek = (TextView) getView().findViewById(R.id.textResultWeek);
+        mTextGivenMonth = (TextView) getView().findViewById(R.id.textGivenMonth);
+        mTextActualMonth = (TextView) getView().findViewById(R.id.textActualMonth);
+        mTextResultMonth = (TextView) getView().findViewById(R.id.textResultMonth);
+        mTextDayOfWeek = (TextView) getView().findViewById(R.id.textDayOfWeek);
+        mTextDayOfMonth = (TextView) getView().findViewById(R.id.textDayOfMonth);
+        mImageStatusDay = (ImageView) getView().findViewById(R.id.imageStatusDay);
+        mImageStatusWeek = (ImageView) getView().findViewById(R.id.imageStatusWeek);
+        mImageStatusMonth = (ImageView) getView().findViewById(R.id.imageStatusMonth);
+//        mTextMonthlyBudget = (TextView) getView().findViewById(R.id.textMonthlyBudget);
+//        mTextDailyBudget = (TextView) getView().findViewById(R.id.textDailyBudget);
+        changeDateTo(new Date());
+    }
+
+    private void changeDateTo(Date date) {
+        mTextStatusDate.setText(dateFormat.format(date));
+
+        RequestParams requestParams = new RequestParams();
+        requestParams.put("date", dateFormatForApi.format(date));
+        progressDialog.show();
+
+        MoneyKeeperRestClientWithAuth.post("state_reports.json", requestParams, mMail, mPass, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d(TAG, "onSuccess " + response.toString());
+                StatusReport report = null;
+                try {
+                     report = mapper.readValue(response.toString(), StatusReport.class);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if(report != null)
+                {
+                    updateViewForReport(report);
+                }
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(int a, Header[] h, String s, Throwable t)
+            {
+                Log.d(TAG, "onFailure " + t.toString());
+                progressDialog.dismiss();
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d(TAG, "onFailure " + errorResponse.toString());
+                progressDialog.dismiss();
+            }
+
+        });
+
+    }
+
+    private void updateViewForReport(StatusReport report) {
+        mTextGivenDay.setText(DecimalFormat.getCurrencyInstance(Locale.GERMANY).format(report.getDailyBudget()));
+        mTextActualDay.setText(DecimalFormat.getCurrencyInstance(Locale.GERMANY).format(report.getDayState()));
+        mTextResultDay.setText(DecimalFormat.getCurrencyInstance(Locale.GERMANY).format(report.getDailyBudget() - report.getDayState()));
+        mTextGivenWeek.setText(DecimalFormat.getCurrencyInstance(Locale.GERMANY).format(report.getWeeklyBudget()));
+        mTextActualWeek.setText(DecimalFormat.getCurrencyInstance(Locale.GERMANY).format(report.getWeekState()));
+        mTextResultWeek.setText(DecimalFormat.getCurrencyInstance(Locale.GERMANY).format(report.getWeeklyBudget() - report.getWeekState()));
+        mTextGivenMonth.setText(DecimalFormat.getCurrencyInstance(Locale.GERMANY).format(report.getMonthlyBudget()));
+        mTextActualMonth.setText(DecimalFormat.getCurrencyInstance(Locale.GERMANY).format(report.getMonthState()));
+        mTextResultMonth.setText(DecimalFormat.getCurrencyInstance(Locale.GERMANY).format(report.getMonthlyBudget() - report.getMonthState()));
+        mTextDayOfWeek.setText(report.getDayOfWeek() +". Tag");
+        mTextDayOfMonth.setText(report.getDayOfMonth() +". Tag");
+
+        mImageStatusDay.setImageResource(getResourceImageForDeviation(report.getDayState(), report.getDailyBudget()));
+        mImageStatusWeek.setImageResource(getResourceImageForDeviation(report.getWeekState(), report.getWeeklyBudget()));
+        mImageStatusMonth.setImageResource(getResourceImageForDeviation(report.getMonthState(), report.getMonthlyBudget()));
+    }
+
+    private int getResourceImageForDeviation(Double state, Double budget) {
+        Double diff = budget - state;
+        if(diff/budget > 0)
+        {
+            return R.drawable.ic_happy;
+        }
+        if(diff/budget < -0.15)
+        {
+            return R.drawable.ic_sad;
+        }
+        return R.drawable.ic_okay;
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        progressDialog =  new ProgressDialog(this.getContext());
         return inflater.inflate(R.layout.fragment_status, container, false);
     }
 
---
+++ app/src/main/java/de/dtonal/moneykeeperapp/StatusReport.java
@@ -0,0 +1,102 @@
+package de.dtonal.moneykeeperapp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
+/**
+ * Created by dtonal on 15.10.16.
+ */
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class StatusReport {
+
+    private Date date;
+    private Double dailyBudget;
+    private Double weeklyBudget;
+    private Double monthlyBudget;
+    private Double dayState;
+    private Double weekState;
+
+    public StatusReport() {
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getDailyBudget() {
+        return dailyBudget;
+    }
+
+    public void setDailyBudget(Double dailyBudget) {
+        this.dailyBudget = dailyBudget;
+    }
+
+    public Double getWeeklyBudget() {
+        return weeklyBudget;
+    }
+
+    public void setWeeklyBudget(Double weeklyBudget) {
+        this.weeklyBudget = weeklyBudget;
+    }
+
+    public Double getMonthlyBudget() {
+        return monthlyBudget;
+    }
+
+    public void setMonthlyBudget(Double monthlyBudget) {
+        this.monthlyBudget = monthlyBudget;
+    }
+
+    public Double getDayState() {
+        return dayState;
+    }
+
+    public void setDayState(Double dayState) {
+        this.dayState = dayState;
+    }
+
+    public Double getWeekState() {
+        return weekState;
+    }
+
+    public void setWeekState(Double weekState) {
+        this.weekState = weekState;
+    }
+
+    public Double getMonthState() {
+        return monthState;
+    }
+
+    public void setMonthState(Double monthState) {
+        this.monthState = monthState;
+    }
+
+    public int getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(int dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public int getDayOfMonth() {
+        return dayOfMonth;
+    }
+
+    public void setDayOfMonth(int dayOfMonth) {
+        this.dayOfMonth = dayOfMonth;
+    }
+
+    private Double monthState;
+    private int dayOfWeek;
+    private int dayOfMonth;
+
+
+}
